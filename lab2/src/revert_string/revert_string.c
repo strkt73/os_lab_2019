77809cc869708192141bcf09fda5bb9e9d2dae17
strkt73 <145946667+strkt73@users.noreply.github.com>
2024-04-20 11:22:18 +0000

    complete

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,23 @@
 #include "revert_string.h"
+#include <stdlib.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+  // вычисляем длину строки
+  int length = strlen(str);
+  // выделяем память для временного хранения перевернутой строки
+  char *temp = (char *)malloc((length + 1) * sizeof(char));
+  // переворачиваем строку, копируя символы с конца в начало
+  for (int i = 0; i < length; ++i)
+  {
+      temp[i] = str[length - i - 1];
+  }
+  // завершаем временную строку нулевым символом
+  temp[length] = '\0';
+  // копируем перевернутую строку обратно в исходную строку
+  strcpy(str, temp);
+  // освобождаем память, выделенную для временной строки
+  free(temp);
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -3,4 +3,7 @@
 void Swap(char *left, char *right)
 {
 	// ваш код здесь
+  char temp = *left;
+  *left = *right;
+  *right = temp;
 }
