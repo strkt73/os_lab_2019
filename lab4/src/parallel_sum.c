ce0da9b3050e651bbbbeb860ca1e48620bc03966
strkt73 <145946667+strkt73@users.noreply.github.com>
2024-04-20 19:27:30 +0000

    complete

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <pthread.h>
+#include "utils.h" // Подключаем библиотеку для генерации массива
 
 struct SumArgs {
   int *array;
@@ -12,7 +12,9 @@ struct SumArgs {
 
 int Sum(const struct SumArgs *args) {
   int sum = 0;
-  // TODO: your code here 
+  for (int i = args->begin; i < args->end; i++) {
+    sum += args->array[i];
+  }
   return sum;
 }
 
@@ -22,39 +24,39 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
+  if (argc != 7) {
+    printf("Usage: %s --threads_num <num> --seed <num> --array_size <num>\n", argv[0]);
+    return 1;
+  }
 
-  uint32_t threads_num = 0;
-  uint32_t array_size = 0;
-  uint32_t seed = 0;
-  pthread_t threads[threads_num];
-
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
+  uint32_t threads_num = atoi(argv[2]);
+  uint32_t seed = atoi(argv[4]);
+  uint32_t array_size = atoi(argv[6]);
 
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed); // Генерируем массив
 
+  pthread_t threads[threads_num];
   struct SumArgs args[threads_num];
+  int step = array_size / threads_num;
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].array = array;
+    args[i].begin = i * step;
+    args[i].end = (i == threads_num - 1) ? array_size : (i + 1) * step;
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
   }
 
   int total_sum = 0;
+  void *result;
   for (uint32_t i = 0; i < threads_num; i++) {
-    int sum = 0;
-    pthread_join(threads[i], (void **)&sum);
-    total_sum += sum;
+    if (pthread_join(threads[i], &result)) {
+      printf("Error: pthread_join failed!\n");
+      return 1;
+    }
+    total_sum += (int)(size_t)result;
   }
 
   free(array);
